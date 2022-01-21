0b892aa17d78662ef98f9d4a2c7b9a5a8ed4ffca
mink0003 <76023265+mink0003@users.noreply.github.com>
2022-01-21 13:13:51 +0800

    Added PrintableQuote class

--- src/PrintableQuote.java
+++ src/PrintableQuote.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class PrintableQuote {
+public class PrintableQuote implements Printable {
+    String stockName;
+    double bid;
+    double ask;
+    double quote;
+    public PrintableQuote(String X, double bid, double ask, double quote){
+        this.stockName = X;
+        this.bid = bid;
+        this.ask = ask;
+        this.quote = quote;
+    }
+    public void print(){
+        System.out.printf("%s BID: %.2f ASK: %.2f LAST: %2f\n", this.stockName, this.bid, this.ask, this.quote);
+    }
 }
