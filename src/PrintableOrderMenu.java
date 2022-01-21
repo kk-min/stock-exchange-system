7b474803465450a2d6ef6a1ee0bbf5b07a9cee53
mink0003 <76023265+mink0003@users.noreply.github.com>
2022-01-21 13:39:54 +0800

    Added PrintableMainMenu class

---
+++ src/PrintableOrderMenu.java
@@ -0,0 +1,12 @@
+public class PrintableOrderMenu implements Printable{
+
+
+    public void print(){
+        System.out.println("Welcome to Min's Simple Stock Exchange Program.");
+        System.out.println("Select an option:");
+        System.out.println("1. Create an Order");
+        System.out.println("2. View Orders");
+        System.out.println("3. Enquire Quote");
+        System.out.println("4. Exit");
+    }
+}
